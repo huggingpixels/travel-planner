ebeede98134df576902ab16365c47cc478e11816
Eszter Szucs-Matyas <eszter.szucs.matyas@gmail.com>
2024-06-27 13:52:08 +0200

    feat(offerForm): :sparkles: layout offer form

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,18 @@
+'use client'
+
 import { title } from "@/components/primitives";
 import { Input } from "@nextui-org/input";
-import {DateRangePicker} from "@nextui-org/date-picker";
-import {Select, SelectSection, SelectItem} from "@nextui-org/select";
+import { DateRangePicker } from "@nextui-org/date-picker";
+import { Select, SelectItem } from "@nextui-org/select";
+
+import { animals } from "./data";
 
 export default function AboutPage() {
   return (
     <div>
-      <h1 className={title()}>About</h1>
+      <h1 className={title()}>Ajánlat generátor béta!</h1>
 
-      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <div className="w-full flex-wrap md:flex-nowrap grid grid-cols-2 gap-4">
         <Input type="email" label="Ügyfél e-mail" />
         <Input label="Ügyfél neve" />
         <DateRangePicker label="Utazás ideje" className="max-w-xs"/>
@@ -19,11 +23,10 @@ export default function AboutPage() {
         <Input label="Indulási hely" />
         <Input label="Egyedi nettó ár" />
         <Select
-        label="Favorite Animal"
+        label="Árazás"
         placeholder="Select an animal"
-        className="max-w-xs"
       >
-        {[{label: "cat", key: ""}].map((animal) => (
+        {animals.map((animal) => (
           <SelectItem key={animal.key}>
             {animal.label}
           </SelectItem>
